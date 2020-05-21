72f9f05d9ad805605b9e13d60b6f96f4e2489db8
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-05-21 16:28:12 -0700

    Added greeting, link to cards and Ballonicorn img

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -4,7 +4,11 @@ class Homepage extends React.Component {
   render() {
     return (
       <div>
-        This component needs to be implemented.
+        <p>Thanks for stopping in. It's so good to have you here!</p>
+        <p>If you'd like to view our really neat trading cards...</p>
+        <a href="/cards">Click Here!</a>
+        <br></br>
+        <img src="/static/img/balloonicorn.jpg"/>
       </div>
     );
   }
